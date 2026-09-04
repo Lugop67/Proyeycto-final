ea028bb472ba7b557445c5fadff42477023357b9
Lugop67 <Lugop67@users.noreply.github.com>
2026-09-04 16:37:31 -0500

    constructores

--- Automovil.java
+++ Automovil.java
@@ -4,5 +4,32 @@ public class Automovil extends Vehiculo {
     private byte numeroPuertas;
     private float capacidadBaulLitros;
     private boolean esSedan;
-}
 
+public Automovil(String marca, int anioFabricacion, double precioBase, boolean estaDisponible, 
+                     String tipoTransmision, byte numeroPuertas, float capacidadBaulLitros, boolean esSedan) {
+        super(marca, anioFabricacion, precioBase, estaDisponible);
+        this.tipoTransmision = tipoTransmision;
+        this.numeroPuertas = numeroPuertas;
+        this.capacidadBaulLitros = capacidadBaulLitros;
+        this.esSedan = esSedan;
+    }
+public Automovil(String marca, int anioFabricacion, double precioBase, 
+                     String tipoTransmision, byte numeroPuertas, float capacidadBaulLitros, boolean esSedan) {
+        super(marca, anioFabricacion, precioBase); 
+        this.tipoTransmision = tipoTransmision;
+        this.numeroPuertas = numeroPuertas;
+        this.capacidadBaulLitros = capacidadBaulLitros;
+        this.esSedan = esSedan;
+    }
+
+    public Automovil(String marca, int anioFabricacion, 
+                     String tipoTransmision, byte numeroPuertas, float capacidadBaulLitros, boolean esSedan) {
+        super(marca, anioFabricacion); 
+        this.tipoTransmision = tipoTransmision;
+        this.numeroPuertas = numeroPuertas;
+        this.capacidadBaulLitros = capacidadBaulLitros;
+        this.esSedan = esSedan;
+
+
+  }   
+}

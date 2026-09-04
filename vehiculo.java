637748669edcd3844945b6eda7b6292b9377b2ea
Lugop67 <Lugop67@users.noreply.github.com>
2026-09-04 16:28:21 -0500

    Getters y Setters

--- vehiculo.java
+++ vehiculo.java
@@ -16,6 +16,41 @@ public class vehiculo {
         this(marca, anioFabricacion, precioBase, true); 
     }
 
+    public vehiculo(String marca, int anioFabricacion) {
+        this(marca, anioFabricacion, 0.0, false);
+    }
+    public String getMarca() { 
+        return marca; 
+    }
+    
+    public void setMarca(String marca) { 
+        this.marca = marca; 
+    }
+
+    public int getAnioFabricacion() { 
+        return anioFabricacion; 
+    }
+    
+    public void setAnioFabricacion(int anioFabricacion) { 
+        this.anioFabricacion = anioFabricacion; 
+    }
+
+    public double getPrecioBase() { 
+        return precioBase; 
+    }
+    
+    public void setPrecioBase(double precioBase) { 
+        this.precioBase = precioBase; 
+    }
+
+    public boolean isEstaDisponible() { 
+        return estaDisponible; 
+    }
+    
+    public void setEstaDisponible(boolean estaDisponible) { 
+        this.estaDisponible = estaDisponible; 
+    }
+
 
 
 

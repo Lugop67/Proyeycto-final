b3692b4fd49225ea17cb7f0e5f04641e079a091a
Lugop67 <Lugop67@users.noreply.github.com>
2026-09-04 16:47:35 -0500

    construcotres

--- Camioneta.java
+++ Camioneta.java
@@ -4,3 +4,32 @@ public class Camioneta extends Vehiculo {
     private char categoriaLicencia;
     private boolean tienePlaton;
 }
+    public Camioneta(String marca, int anioFabricacion, double precioBase, boolean estaDisponible, 
+                     String tipoTraccion, long capacidadCargaKg, char categoriaLicencia, boolean tienePlaton) {
+        super(marca, anioFabricacion, precioBase, estaDisponible);
+        this.tipoTraccion = tipoTraccion;
+        this.capacidadCargaKg = capacidadCargaKg;
+        this.categoriaLicencia = categoriaLicencia;
+        this.tienePlaton = tienePlaton;
+    }
+
+    public Camioneta(String marca, int anioFabricacion, double precioBase, 
+                     String tipoTraccion, long capacidadCargaKg, char categoriaLicencia, boolean tienePlaton) {
+        super(marca, anioFabricacion, precioBase);
+        this.tipoTraccion = tipoTraccion;
+        this.capacidadCargaKg = capacidadCargaKg;
+        this.categoriaLicencia = categoriaLicencia;
+        this.tienePlaton = tienePlaton;
+    }
+
+    public Camioneta(String marca, int anioFabricacion, 
+                     String tipoTraccion, long capacidadCargaKg, char categoriaLicencia, boolean tienePlaton) {
+        super(marca, anioFabricacion);
+        this.tipoTraccion = tipoTraccion;
+        this.capacidadCargaKg = capacidadCargaKg;
+        this.categoriaLicencia = categoriaLicencia;
+        this.tienePlaton = tienePlaton;
+    }
+    
+        
+    

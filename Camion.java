b82b6308a9d36bf620799cf5fecab6f47a68b67f
Lugop67 <Lugop67@users.noreply.github.com>
2026-09-04 16:56:23 -0500

    get y set clase camion

--- Camion.java
+++ Camion.java
@@ -19,3 +19,51 @@ public class Camion extends Vehiculo {
         this.capacidadCargaToneladas = capacidadCargaToneladas;
         this.tieneCamaron = tieneCamaron;
     }
+    public Camion(String marca, int anioFabricacion, String tipoCarroceria, byte numeroEjes, double capacidadCargaToneladas, boolean tieneCamaron) {
+        super(marca, anioFabricacion);
+        this.tipoCarroceria = tipoCarroceria;
+        this.numeroEjes = numeroEjes;
+        this.capacidadCargaToneladas = capacidadCargaToneladas;
+        this.tieneCamaron = tieneCamaron;
+    }
+    public String getTipoCarroceria() {
+        return tipoCarroceria;
+    }
+
+    public void setTipoCarroceria(String tipoCarroceria) {
+        this.tipoCarroceria = tipoCarroceria;
+    }
+
+    public byte getNumeroEjes() {
+        return numeroEjes;
+    }
+
+    public void setNumeroEjes(byte numeroEjes) {
+        this.numeroEjes = numeroEjes;
+    }
+
+    public double getCapacidadCargaToneladas() {
+        return capacidadCargaToneladas;
+    }
+
+    public void setCapacidadCargaToneladas(double capacidadCargaToneladas) {
+        this.capacidadCargaToneladas = capacidadCargaToneladas;
+    }
+
+    public boolean isTieneCamaron() {
+        return tieneCamaron;
+    }
+
+    public void setTieneCamaron(boolean tieneCamaron) {
+        this.tieneCamaron = tieneCamaron;
+    }
+
+    @Override
+    public String toString() {
+        return "CAMIÓN -> " + super.toString() + 
+               " | Carrocería: " + tipoCarroceria + 
+               " | Ejes: " + numeroEjes + 
+               " | Carga: " + capacidadCargaToneladas + " Tn" + 
+               " | Camarote: " + (tieneCamaron ? "Sí" : "No");
+    }
+}

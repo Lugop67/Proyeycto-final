da81c2c4743d37188c1a8a4988e25dd6cc1e0c8a
Lugop67 <Lugop67@users.noreply.github.com>
2026-09-04 16:40:50 -0500

    constructores

--- Motocicleta.java
+++ Motocicleta.java
@@ -4,3 +4,28 @@ public class Motocicleta extends Vehiculo {
     private short cilindraje;
     private float pesoKg;
     private boolean incluyeCascoXtrong;
+
+    public Motocicleta(String marca, int anioFabricacion, double precioBase, boolean estaDisponible, String tipoGasolina, short cilindraje, float pesoKg, boolean incluyeCascoXtrong) {
+        super(marca, anioFabricacion, precioBase, estaDisponible);
+        this.tipoGasolina = tipoGasolina;
+        this.cilindraje = cilindraje;
+        this.pesoKg = pesoKg;
+        this.incluyeCascoXtrong = incluyeCascoXtrong;
+    }
+
+    public Motocicleta(String marca, int anioFabricacion, double precioBase, String tipoGasolina, short cilindraje, float pesoKg, boolean incluyeCascoXtrong) {
+        super(marca, anioFabricacion, precioBase);
+        this.tipoGasolina = tipoGasolina;
+        this.cilindraje = cilindraje;
+        this.pesoKg = pesoKg;
+        this.incluyeCascoXtrong = incluyeCascoXtrong;
+    }
+
+
+    public Motocicleta(String marca, int anioFabricacion, String tipoGasolina, short cilindraje, float pesoKg, boolean incluyeCascoXtrong) {
+        super(marca, anioFabricacion);
+        this.tipoGasolina = tipoGasolina;
+        this.cilindraje = cilindraje;
+        this.pesoKg = pesoKg;
+        this.incluyeCascoXtrong = incluyeCascoXtrong;
+    }

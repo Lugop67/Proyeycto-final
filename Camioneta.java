f57078767e6c9f09814feb09834f4f4a7f35b4f2
Lugop67 <Lugop67@users.noreply.github.com>
2026-09-04 16:43:48 -0500

    classe camioneta

---
+++ Camioneta.java
@@ -0,0 +1,6 @@
+public class Camioneta extends Vehiculo {
+    private String tipoTraccion;
+    private long capacidadCargaKg;
+    private char categoriaLicencia;
+    private boolean tienePlaton;
+}
